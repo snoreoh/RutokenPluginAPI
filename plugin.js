c0215ca3a191d356328036c51c8073b50c3947f7
snoreoh <nikita.hrapov@mail.ru>
2023-08-02 20:33:39 +0300

    upgrade server.py and plugin.js

--- plugin.js
+++ plugin.js
@@ -7,76 +7,44 @@ class CryptoPlugin{
     }
 
     async enumerateDevices(options){
-        return await this.request(JSON.stringify({
-            "operationID": "enumerateDevices",
-            "parameters": {
-                'options': options
-            }}))
+        return await this.request("enumerateDevices", Array.prototype.slice.call(arguments))
     }
 
     async enumerateKeys(deviceId, marker = ''){
-        return await this.request(JSON.stringify({
-            "operationID": "enumerateKeys",
-            "parameters": {
-                'deviceId': deviceId,
-                'marker': marker
-            }})).then(data =>{
-                return data["keys"]
-            })
+        return await this.request("enumerateKeys", Array.prototype.slice.call(arguments))
     }
 
     async enumerateCertificates(deviceId, category = 'CERT_CATEGORY_UNSPEC'){
-        return await this.request(JSON.stringify({ 
-            "operationID": "enumerateCertificates",
-            "parameters": {
-                'deviceId': deviceId,
-                'category': category
-            }})).then(data => {
-                return data['certificates']
-            })
+        return await this.request("enumerateCertificates", Array.prototype.slice.call(arguments))
     }
 
     async getDeviceInfo(deviceId, option = 'TOKEN_INFO_MODEL'){
-        return await this.request( JSON.stringify({
-            "operationID": "getDeviceInfo",
-            "parameters": {
-                'deviceId': deviceId,
-                'option': option
-            }}))
+        return await this.request("getDeviceInfo", Array.prototype.slice.call(arguments))
     }
 
     async getKeyLabel(deviceId, keyId){
-        return await this.request(JSON.stringify({
-            "operationID": "getKeyLabel",
-            "parameters": {
-                'deviceId': deviceId,
-                'keyId': keyId
-            }})).then(data =>{
-                return data["label"]
-            })
+        return await this.request("getKeyLabel", Array.prototype.slice.call(arguments))
     }
 
     async parseCertificate(deviceId, certId){
-        return await this.request(JSON.stringify({
-            "operationID": "parseCertificate",
-            "parameters": {
-                'deviceId': deviceId,
-                'certId': certId
-            }}))
+        return await this.request("parseCertificate", Array.prototype.slice.call(arguments))
     }
 
     //Create request to the server and handle errors from it
-    async request(jsonData){
+    async request(funcName, funcParameters){
         return await fetch('http://127.0.0.1:5000/', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json, text/plain, */*', 
                 'Content-Type': 'application/json' 
             },
-            body: jsonData
+            body: JSON.stringify({
+                "operationId": arguments[0],
+                "arguments": arguments[1]
+            })
         }).then(response => {
             if(!response.ok){
-                return Promise.reject(new Error('Response failed: ' + response.status + '(' + response.statusText + ')'));
+                return Promise.reject(new Error('6'));
             }
             return response.json();
         }).catch(error => {
@@ -85,15 +53,13 @@ class CryptoPlugin{
             if ('message' in respData){
                 return Promise.reject(new Error(respData['message']))    
             }
-            return respData;
+            return respData['response'];
         })
     }
-
 }
 
 // Test Input
-//_plugin = new CryptoPlugin();
-
+_plugin = new CryptoPlugin();
 // _plugin.enumerateDevices("ENUMERATE_DEVICES_EVENTS").then(data =>{
 //     alert(data["connected"])
 // }, function(error){
@@ -110,7 +76,7 @@ class CryptoPlugin{
 //     alert(error.message)
 // });
 // _plugin.getDeviceInfo(852752315).then(data => {
-//     alert(data['info'])
+//     alert(data)
 // }, function(error){
 //     alert(error.message)
 // });
@@ -120,7 +86,7 @@ class CryptoPlugin{
 //     alert(error.message)
 // });
 // _plugin.parseCertificate(852752315, "reer6f56a65a5c6awfd").then(data => {
-//     alert(data['category'])
+//     alert(data[0])
 // }, function(error){
 //     alert(error.message)
 // });
--- server/server.py
+++ server/server.py
@@ -2,44 +2,44 @@ from flask import Flask, request, make_response
 from flask_cors import cross_origin
 import json
 
-app = Flask(__name__)
 
+class plugin:
 
-'''
-List of error codes:
-'''
-FUNCTION_FAILED = "1"
-DEVICE_NOT_FOUND = "2"
-KEY_NOT_FOUND = "3"
-CERTIFICATE_CATEGORY_BAD = "4"
-CERTIFICATE_NOT_FOUND = "5"
+    def __init__(self) -> None:
+        self.jsonData = {}
+        self.errorCodes = {
+            'FUNCTION_FAILED': "1",
+            'DEVICE_NOT_FOUND': "2",
+            'KEY_NOT_FOUND': "3",
+            'CERTIFICATE_CATEGORY_BAD': "4",
+            'CERTIFICATE_NOT_FOUND': "5" 
+        }
+    
+    def setJsonData(self, file):
+        try:
+            f = open(f'{file}.json')
+            self.jsonData = json.load(f)
+            f.close()
+            return 0
+        except:
+            return self.errorResponse('FUNCTION_FAILED')   
 
-
-@app.route('/', methods = ['POST'])
-@cross_origin(headers=['Content-Type'])
-def response():
-
-    req = request.get_json()
-    operationId = req['operationID']
-
-    try:
-        f = open(f"{operationId}.json")
-        jsonData = json.load(f)
-    except:
+    def errorResponse(self, error):
         return json.dumps({
-            "message": FUNCTION_FAILED
-        })  
+            'message': self.errorCodes[error]
+        })
     
-    if operationId == "enumerateDevices":
-        return jsonData[req['parameters']['options']]
+    def response(self, response):
+        return json.dumps({
+            "response": response
+        })
     
-    deviceId = req['parameters']['deviceId']
+    def enumerateDevices(self, options):
+        return self.response(self.jsonData[options])
 
-    if operationId == "enumerateKeys":
-
-        marker = req['parameters']['marker']
-
-        for device in jsonData:
+    
+    def enumerateKeys(self, deviceId, marker = ''):
+        for device in self.jsonData:
             if device['deviceId'] == deviceId:
                 keys = []
                 for key in device['keys']:
@@ -50,29 +50,17 @@ def response():
                     keys.append(key['keyId'])
 
                 if not keys:
-                    return json.dumps({
-                        "message": KEY_NOT_FOUND
-                    })
+                    return self.errorResponse('KEY_NOT_FOUND')
                 
-                return json.dumps({
-                    'keys' : keys
-                })
+                return self.response(keys)
 
-        return json.dumps({
-            "message": DEVICE_NOT_FOUND
-        })
-        
-    
-    elif operationId == "enumerateCertificates":
+        return self.errorResponse('DEVICE_NOT_FOUND')
 
-        category = req['parameters']['category']
-        
+    def enumerateCertificates(self, deviceId, category = 'CERT_CATEGORY_UNSPEC'):
         if category not in ["CERT_CATEGORY_UNSPEC", "CERT_CATEGORY_USER", "CERT_CATEGORY_CA", "CERT_CATEGORY_OTHER"]:
-            return json.dumps({
-                        "message": CERTIFICATE_CATEGORY_BAD
-                    })
+            return self.errorResponse('CERTIFICATE_CATEGORY_BAD')
         
-        for device in jsonData:
+        for device in self.jsonData:
             
             if device['deviceId'] == deviceId:
                 certificates = []
@@ -82,77 +70,67 @@ def response():
                             certificates.append(certificate['certificateId'])
                         continue
                     certificates.append(certificate["certificateId"])
-                                                   ##certificateId
                 
                 if not certificates:
-                    return json.dumps({
-                        "message": CERTIFICATE_NOT_FOUND
-                    })
+                    return self.errorResponse('CERTIFICATE_NOT_FOUND')
                 
-                return json.dumps({
-                    'certificates' : certificates
-                })
-
-        return json.dumps({
-            "message": DEVICE_NOT_FOUND
-        })
-
-    elif operationId == "getDeviceInfo":
-
-        option = req['parameters']['option']
-
-        for device in jsonData:
-            if device['deviceId'] == deviceId:
-                    return json.dumps({
-                        'info': device['info'][option]
-                    })
+                return self.response(certificates)
             
-        return json.dumps({
-            "message": DEVICE_NOT_FOUND
-        })
+        return self.errorResponse('DEVICE_NOT_FOUND')
+
+    def getDeviceInfo(self, deviceId, option = 'TOKEN_INFO_MODEL'):
+        for device in self.jsonData:
+            if device['deviceId'] == deviceId:
+                    return self.response(device['info'][option])
+            
+        return self.errorResponse('DEVICE_NOT_FOUND')
 
 
-    elif operationId == "getKeyLabel":
-
-        keyId = req['parameters']['keyId']
-
-        for device in jsonData:
+    def getKeyLabel(self, deviceId, keyId):
+        for device in self.jsonData:
             if device['deviceId'] == deviceId:
                 for key in device['keys']:
                     if key['keyId'] == keyId:
-                        return json.dumps({
-                            'label': key['label']
-                        }) 
+                        return self.response(key['label'])
                     
-                return json.dumps({
-                    "message": KEY_NOT_FOUND
-                })
-            
-        return json.dumps({
-            "message": DEVICE_NOT_FOUND
-        })
-    
-    elif operationId == "parseCertificate":
+                return self.errorResponse('KEY_NOT_FOUND')
         
-        certificateId = req['parameters']['certId']
+        return self.errorResponse('DEVICE_NOT_FOUND')
 
-        for device in jsonData:
+
+    def parseCertificate(self, deviceId, certId):
+        for device in self.jsonData:
             if device['deviceId'] == deviceId:
                 for certificate in device['certificates']:
-                    if certificate['certificateId'] == certificateId:
-                        return json.dumps({
-                            "category": certificate['category'],
-                            "handle": certificate['handle']
-                        }) 
+                    if certificate['certificateId'] == certId:
+                        return self.response([
+                            certificate['category'],
+                            certificate['handle']
+                        ])
                 
-                return json.dumps({
-                    "message": CERTIFICATE_NOT_FOUND
-                })
+                return self.errorResponse('CERTIFICATE_NOT_FOUND')
             
-        return json.dumps({
-            "message": DEVICE_NOT_FOUND
-        })
-        
+        return self.response('DEVICE_NOT_FOUND')
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+@cross_origin(headers=['Content-Type'])
+def response():
+
+    req = request.get_json()
+    reqData = [req[key] for key in req]
+    
+    _plugin = globals()['plugin']()
+    functionFile = getattr(_plugin, 'setJsonData')(reqData[0])
+
+    if functionFile == 0:
+        return getattr(_plugin, f'{reqData[0]}')(*tuple(reqData[1]))
+    else:
+        return functionFile
+    
 
 if __name__ == '__main__':
     app.run()
